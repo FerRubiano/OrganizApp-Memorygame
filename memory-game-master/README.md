759c233e0316c9f897b01ae8a1163a7428d25c98
FerRubiano <millos012@gmail.com>
2021-12-08 14:45:08 -0500

    Juego de memoria OrganizApp

---
+++ memory-game-master/README.md
@@ -0,0 +1,40 @@
+# MemoryGame
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.3.
+
+---
+
+## Description
+
+This is a match pairs game with three difficult levels. You must pair each image with their equal.
+
+---
+
+## Run the app
+
+### Local machine
+
+For run the app using the local machine as an app server, after cloning the project in a local folder, you must start the server, into the project folder, using the command:
+
+`ng serve`
+
+Then, you have to navigate to the address:
+
+`http://localhost:4200`
+
+
+### In the web
+
+You can run the app navigating to this URL:
+
+[demo](https://www.guitxo.com/juego-de-memoria-con-angular-9/)
+
+---
+
+## Screenshot
+
+![Memory Game - Screenshot](./screenshot.png "Memory Game - Screenshot")
+
+
+
+
---
+++ memory-game-master/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { GameService } from './services/game.service';
+import { Observable } from 'rxjs';
+import { ModalDialogService } from './modules/modal-dialog/modal-dialog.service';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  /**
+   * constructor method
+   * @param gameService instance for the services that manage the game
+   * @param modalDialogService instance for the service of the model dialog
+   * @param translate instance for the service of translations
+   */
+  constructor(
+    private gameService: GameService,
+    private modalDialogService: ModalDialogService,
+    private translate: TranslateService,
+  ) {}
+
+  /**
+   * ngOnInit inits a subscription to control when user wins the game
+   */
+  ngOnInit(): void {
+    this.gameService.winedGame.subscribe(
+      (value) => {
+        if (value) {
+          this.modalDialogService.newDialog({
+            title: this.translate.instant('finish-message.title'),
+            message: this.translate.instant('finish-message.win-game'),
+            clickOverlayCloses: true,
+          });
+          this.modalDialogService.openDialog();
+        }
+      }
+    );
+  }
+
+  /**
+   * returns if the game is started or not
+   */
+  getGameStarted(): Observable<boolean> {
+    return this.gameService.started;
+  }
+}
